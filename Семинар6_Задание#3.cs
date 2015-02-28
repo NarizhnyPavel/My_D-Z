937071ada9bd88c9aceea683116aa735477997df
narizhypavel145 <narizhy@mail.ru>
2015-02-28 14:56:58 +0300

    Update Семинар6_Задание#3.cs

--- Семинар6_Задание#3.cs
+++ Семинар6_Задание#3.cs
@@ -9,19 +9,22 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-          int[] y1 = new int[5];
-          int[] y2 = new int[5];
-          for (int i = 0; i <= 4; i++)
-          { 
-             y1[i]==2 * i +2;
-             y2[i]==Math.Pow(i,2);
-          }
-          Console.WriteLine("таблица значений для 1-ой функции");
-          Console.Write("x");
-          for(int x = 0;x <= 4; x++) Console.Write(x + "|");
-          Console.WriteLine("|");
-          Console.WriteLine("y");
-          for(int y = 0; y <=4; y++) Console.Write((2 * y + 2) + "|");
-          Console.WriteLine("|");
-          
-          
+            Console.WriteLine("таблица значений для 1-ой функции");
+            Console.Write("x|");
+            for (int x = 0; x <= 4; x++) Console.Write(x + "|");
+            Console.WriteLine();
+            Console.Write("y|");
+            for (int y = 0; y <= 4; y++) Console.Write((2 * y + 1) + "|");
+            Console.WriteLine();
+
+            Console.WriteLine("Таблица значений для 2-ой функции");
+            Console.Write("x|");
+            for (int x = 0; x <= 4; x++) Console.Write(x + "|");
+            Console.WriteLine();
+            Console.Write("y|");
+            for (int y = 0; y <= 4; y++) Console.Write((Math.Pow(y,2)) + "|");
+            Console.WriteLine();
+            Console.ReadLine();
+        } 
+    }
+}
