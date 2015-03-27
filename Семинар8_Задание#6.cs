d29c874b7b7018e7e6e35e35f81ab70cd7b4f9b0
narizhypavel145 <narizhy@mail.ru>
2015-03-27 11:24:18 +0300

    Update Семинар8_Задание#6.cs

--- Семинар8_Задание#6.cs
+++ Семинар8_Задание#6.cs
@@ -9,9 +9,9 @@ static void Main(string[] args)
             Random rand = new Random();
             int[] mas = new int[4];
             mas[0] = rand.Next(97, 122);
-            mas[1] = rand.Next(1,9);
+            mas[1] = rand.Next(0,9);
             mas[2] = rand.Next(97, 122);
-            mas[3] = rand.Next(1,9);
+            mas[3] = rand.Next(0,9);
             
             Char b;
             Char[] mass = new Char[4];
